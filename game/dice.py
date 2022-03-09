18478a308c5a027e542f808c56d3e86a4a126ad3
adam_holgersson <adam.holgersson@hotmail.se>
2022-03-09 16:55:16 +0100

    A function that resets rolls_made to 0 added (clean_rolls_made)

--- game/dice.py
+++ game/dice.py
@@ -22,9 +22,16 @@ class Dice():
     def get_rolls_made(self):
         return self.rolls_made
 
+    def clean_rolls_made (self):
+        self.rolls_made = 0
+        return self.rolls_made
+
 
 
 if __name__ == '__main__':
     die = Dice()
     roll = die.roll_dice()
     print(roll)
+    print(die.get_rolls_made())
+    die.clean_rolls_made()
+    print(die.get_rolls_made())
