244470cccc9d6eba6aa549a65c8db3b7fab7e70b
David Trang <dtrang49@gmail.com>
2022-03-09 16:26:21 +0100

    first instance of display graphics and test_display_graphics is implemented

--- test/display_test.py
+++ test/display_test.py
@@ -5,11 +5,18 @@ from game import display
 
 class TestDisplay(unittest.TestCase):
  
-    def test_init_default_object(self):
+    def test_display_rules(self):
         displayer = display.Display()
         rules = displayer.display_rules()
+        
         exp = ['1. Do not cheat', '2. Do not swear', '3. Do not fight', '4. Have fun']
         self.assertListEqual(rules,exp)
+    
+    def test_display_graphics(self):
+        displayer = display.Display()
+        blank = ''
+        self.assertNotEqual(blank,displayer.display_graphics)
+        
         
 if __name__ == '__main__':
     unittest.main()
